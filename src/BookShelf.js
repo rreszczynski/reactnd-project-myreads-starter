451db79986a0ba614f44416e5f1bfbc9389a245c
Rafał Reszczyński <reszczynski.r@gmail.com>
2018-07-28 21:00:32 +0200

    bookshelf change to stateless functional component

--- src/BookShelf.js
+++ src/BookShelf.js
@@ -2,27 +2,25 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import Book from './Book.js'
 
-class BookShelf extends React.Component {
-	static propTypes = {
+const BookShelf = (props) => {
+	BookShelf.propTypes = {
 		bookShelfTilte: PropTypes.string.isRequired,
 		books: PropTypes.array.isRequired,
 		changeShelf: PropTypes.func.isRequired
 	}
 	
-	render() {
-		return (
-			<div className="bookshelf">
-				<h2 className="bookshelf-title">{this.props.bookShelfTilte}</h2>
-				<div className="bookshelf-books">
+	return (
+		<div className="bookshelf">
+			<h2 className="bookshelf-title">{props.bookShelfTilte}</h2>
+			<div className="bookshelf-books">
 				<ol className="books-grid">
-					{this.props.books.map((book) => (
-								<Book key={book.id} book={book} changeShelf={this.props.changeShelf}/>
+					{props.books.map((book) => (
+						<Book key={book.id} book={book} changeShelf={props.changeShelf}/>
 					))}
 				</ol>
 			</div>
-			</div>
-		)
-	}
+		</div>
+	)
 }
 
 export default BookShelf
